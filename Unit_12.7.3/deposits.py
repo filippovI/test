fc476bc4fcd3cfb735f668c4d29d1d1fa67bd7b1
Ivan <filippovania66@gmail.com>
2021-11-28 22:08:03 +0500

    add files

---
+++ Unit_12.7.3/deposits.py
@@ -0,0 +1,14 @@
+import wheel
+
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+money = float(input("Введите сумму, которую хотите внести: "))
+per_cent['ТКБ'] = money * per_cent.get('ТКБ') / 100
+per_cent['СКБ'] = money * per_cent.get('СКБ') / 100
+per_cent['ВТБ'] = money * per_cent.get('ВТБ') / 100
+per_cent['СБЕР'] = money * per_cent.get('СБЕР') / 100
+per_cent = {k: round(v, 2) for k, v in per_cent.items()}
+for k, v in per_cent.items():
+    print(k, '-', v)
+print('Максимальная сумма, которую вы можете заработать - ' + str(max(per_cent.values())))
+# Для консольки
+# program_exit = input("Press enter to exit ")
