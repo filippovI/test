87823964eac1ac20afb3e734872fcf8a92e2aeda
filippovI <93511996+filippovI@users.noreply.github.com>
2021-11-29 18:39:13 +0500

    up 1.1
    upgrade

--- Unit_12.7.3/deposits.py
+++ Unit_12.7.3/deposits.py
@@ -1,12 +1,8 @@
 per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
 money = float(input("Введите сумму, которую хотите внести: "))
 for k, v in per_cent.items():
-    per_cent[k] = money * per_cent.get(k) / 100
-
-per_cent = {k: round(v, 2) for k, v in per_cent.items()}
-
-for k, v in per_cent.items():
-    print(k, '-', v)
+    per_cent[k] = round(money * per_cent.get(k) / 100, 2)
+    print(k, '-', per_cent.get(k))
 print('Максимальная сумма, которую вы можете заработать - ' + str(max(per_cent.values())))
 # Для консольки
 # program_exit = input("Press enter to exit ")
