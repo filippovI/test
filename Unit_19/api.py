a5eef655d594d7f68ba6c19a03ab5974093ad1fb
Ivan <filippovania66@gmail.com>
2022-02-10 16:43:43 +0500

    the new project

--- Unit_19/api.py
+++ Unit_19/api.py
@@ -4,32 +4,30 @@ from requests_toolbelt.multipart.encoder import MultipartEncoder
 
 class PetFriends:
     def __init__(self):
-        self.url = 'https://petfriends1.herokuapp.com/'
+        self.__url = 'https://petfriends1.herokuapp.com/'
 
-    def get_api_key(self, email: str, password: str) -> [int, str | dict]:
+    def get_api_key(self, email: str, password: str):
+        """Метод для получения API ключа"""
+
         headers = {
             'email': email,
             'password': password
         }
-        res = rq.get(self.url + 'api/key', headers=headers)
-        try:
-            result = res.json()
-        except:
-            result = res.text
-        return res.status_code, result
+        res = rq.get(self.__url + 'api/key', headers=headers)
+        return PetFriends.__get_result(res)
 
-    def get_pets(self, key: str, flr: str = '') -> [int, str | dict]:
+    def get_pets(self, key: str, flr: str = ''):
+        """Метод для получения списка питомцев. flt = 'my_pets' - только свои питомцы"""
+
         headers = {'auth_key': key}
         flr = {'filter': flr}
 
-        res = rq.get(self.url + 'api/pets', headers=headers, params=flr)
-        try:
-            result = res.json()
-        except:
-            result = res.text
-        return res.status_code, result
+        res = rq.get(self.__url + 'api/pets', headers=headers, params=flr)
+        return PetFriends.__get_result(res)
 
-    def add_pet(self, name: str, animal_type: str, age: str, key: str, pet_photo: str) -> [int, str | dict]:
+    def add_pet(self, name: str, animal_type: str, age: str, key: str, pet_photo: str):
+        """Метод для добавления питомца на сайт"""
+
         data = MultipartEncoder(
             fields={
                 'name': name,
@@ -43,15 +41,33 @@ class PetFriends:
             'Content-Type': data.content_type
         }
 
-        res = rq.post(self.url + 'api/pets', headers=headers, data=data)
-        try:
-            result = res.json()
-        except:
-            result = res.text
-        return res.status_code, result
+        res = rq.post(self.__url + 'api/pets', headers=headers, data=data)
+        return PetFriends.__get_result(res)
 
-    def del_pet(self, key: str, pet_id: str) -> [int, str | dict]:
-        res = rq.delete(self.url + 'api/pets/{0}'.format(pet_id), headers={'auth_key': key})
+    def update_pet(self, key: str, pet_id: str, name: str = '', animal_type: str = '', age: int = ''):
+        """Метод для изменения информации о питомце"""
+
+        headers = {
+            'auth_key': key
+        }
+        data = {
+            'name': name,
+            'animal_type': animal_type,
+            'age': age
+        }
+        res = rq.put(self.__url + 'api/pets/{0}'.format(pet_id), headers=headers, data=data)
+        return PetFriends.__get_result(res)
+
+    def del_pet(self, key: str, pet_id: str):
+        """Метод для удаления питомца с сайта"""
+
+        res = rq.delete(self.__url + 'api/pets/{0}'.format(pet_id), headers={'auth_key': key})
+        return PetFriends.__get_result(res)
+
+    @staticmethod
+    def __get_result(res) -> [int, str | dict]:
+        """Метод для возврата результатов методов"""
+
         try:
             result = res.json()
         except:
--- Unit_19/test.py
+++ Unit_19/test.py
@@ -3,7 +3,4 @@ import settings as s
 
 pf = PetFriends()
 _, key = pf.get_api_key(s.email, s.password)
-_, my_pets = pf.get_pets(key['key'], 'my_pets')
-status, my_del_pets = pf.del_pet(key['key'], my_pets['pets'][0]['id'])
-print(status, my_del_pets)
-
+_, add_pet = pf.add_pet('Котэ', 'cat', '22', key['key'], 'tests/images/cat.jpg')
