7434318cd7b58265975a7739c6f8333efec47953
Ivan <filippovania66@gmail.com>
2022-02-14 14:14:07 +0500

    -delete test file

--- Unit_19.7.2/test.py
+++
@@ -1,9 +0,0 @@
-from api import PetFriends
-import settings as s
-
-pf = PetFriends()
-
-_, key = pf.get_api_key(s.email, s.password)
-status, result = pf.del_pet(key['key'], 'jhgjkl')
-
-print(status, result)
